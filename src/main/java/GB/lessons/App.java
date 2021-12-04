7212498630a6b617c5fb944deb023bd0e0a7dc36
olgasuperstars <olgasuperstar@mail.ru>
2021-12-04 20:04:06 +0300

    Lesson 3

--- src/main/java/GB/lessons/App.java
+++ src/main/java/GB/lessons/App.java
@@ -1,13 +1,70 @@
 package GB.lessons;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.time.LocalDate;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+    public static void main(String[] args ) throws InterruptedException {
+        WebDriver webDriver = WebDriverManager.chromedriver().create();
+
+        webDriver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        webDriver.get("https://crm.geekbrains.space/");
+        webDriver.manage().window().setSize(new Dimension(1980, 1020));
+        webDriver.findElement(By.name("_username")).sendKeys("Applanatest1");
+        webDriver.findElement(By.name("_password")).sendKeys("Student2020!");
+        webDriver.findElement(By.name("_submit")).click();
+
+
+        Actions actions = new Actions(webDriver);
+
+        actions.moveToElement(webDriver.findElement(By.xpath("//a[./span[text()='Проекты']]")))
+                  .build().perform();
+
+        webDriver.findElement(By.xpath("//a[./span[text()='Мои проекты']]")).click();
+
+        new WebDriverWait(webDriver, 10, 500)
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[text()='Создать проект']")))
+                .click();
+
+        webDriver.findElement(By.name("crm_project[name]")).sendKeys("Орг" + UUID.randomUUID());
+
+        new Select(webDriver.findElement(By.xpath("//label[text()='Приоритет']/../following-sibling::div//select"))).selectByVisibleText("Средний");
+        new Select(webDriver.findElement(By.xpath("//label[text()='Финансирование']/../following-sibling::div//select"))).selectByVisibleText("Внутреннее");
+        new Select(webDriver.findElement(By.xpath("//label[text()='Подразделение']/../following-sibling::div//select"))).selectByVisibleText("Research & Development");
+        new Select(webDriver.findElement(By.xpath("//label[text()='Куратор проекта']/../following-sibling::div//select"))).selectByVisibleText("Ким Юрий");
+        new Select(webDriver.findElement(By.xpath("//label[text()='Руководитель проекта']/../following-sibling::div//select"))).selectByVisibleText("Марков Глеб");
+        new Select(webDriver.findElement(By.xpath("//label[text()='Менеджер']/../following-sibling::div//select"))).selectByVisibleText("Палкина Анна");
+
+        new OrganizationDropDownList(webDriver).selectOrganization("GeekBrains1");
+
+        webDriver.findElement(By.xpath("//button[contains(text(),'Сохранить') and not(contains(text(), 'закрыть'))]")).click();
+        new WebDriverWait(webDriver, 8).until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(text(), 'Проект сохранен')]")));
+
+        Thread.sleep(5000);
+
+        webDriver.quit();
     }
 }
---
+++ src/main/java/GB/lessons/OrganizationDropDownList.java
@@ -0,0 +1,11 @@
+package GB.lessons;
+
+import org.openqa.selenium.WebDriver;
+
+public class OrganizationDropDownList {
+    public OrganizationDropDownList(WebDriver webDriver) {
+    }
+
+    public void selectOrganization(String geekBrains1) {
+    }
+}
