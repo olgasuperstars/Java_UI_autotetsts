53588b163cee0b50c8e70e993fabb18811b1a205
olgasuperstars <olgasuperstar@mail.ru>
2021-12-12 22:18:49 +0300

    Lesson 4

--- src/main/java/GB/lessons/lesson4/Triangle.java
+++ src/main/java/GB/lessons/lesson4/Triangle.java
@@ -7,33 +7,42 @@ import lombok.*;
 @AllArgsConstructor
 public class Triangle {
 
-        private int a;
-        private int b;
-        private int c;
-        private Colour colour = Colour.WHITE;
+    private int a;
+    private int b;
+    private int c;
+    private Colour colour = Colour.WHITE;
 
-        public Triangle(int a, int b, int c) {
-                this.a = a;
-                this.b = b;
-                this.c = c;
-        }
 
-        public int calculatePerimeter() {
-                checkSidesArePositive();
-                checkValid();
-                return a + b + c;
-        }
-        private void checkSidesArePositive() {
-                if (a <= 0 || b <= 0 || c <= 0) {
-                        throw new IllegalArgumentException("The sides must be positive");
-                }
-        }
-        private void checkValid() {
-                long max = Math.max(Math.max(a, b), c);
-                if (max >= (a + b + c - max)) {
-                        throw new IllegalArgumentException("The any two sides must be greater than other side");
-                }
-        }
+    public Triangle(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
 
+    public int calculatePerimeter() {
+        checkSidesArePositive();
+        checkValid();
+        return a + b + c;
+
+    }
+
+    private void checkSidesArePositive() {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalArgumentException("The sides must be positive");
+        }
+    }
+
+    private void checkValid() {
+        long max = Math.max(Math.max(a, b), c);
+        if (max >= (a + b + c - max)) {
+            throw new IllegalArgumentException("The any two sides must be greater than other side");
+        }
+    }
+
+    public double calcArea() {
+        double halfPerimeter = calculatePerimeter() / 2;
+        return Math.sqrt(halfPerimeter*(halfPerimeter-a)*(halfPerimeter-b)*halfPerimeter-c);
+
+    }
 
 }
